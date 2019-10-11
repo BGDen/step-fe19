39cf734c8ff00b293c52342c738b86e728d04297
Николай Сотула <sotula@itstep1.dp.ua>
2019-10-11 21:14:38 +0300

    localstorage

---
+++ 20191011_localStorage/app/main.js
@@ -0,0 +1,18 @@
+let a = 4;
+const b = {
+    name : 'Ivan'
+},
+d = [222, 333],
+e = ()=>2;
+
+class F{
+    constructor(){
+        //haha
+    }
+}
+
+console.log(b, typeof b, typeof d, typeof e, typeof F);
+let c = JSON.stringify(b);
+console.log(c, typeof c);
+
+
