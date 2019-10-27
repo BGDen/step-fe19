00149292c1510d6ce5a6c599833f1da9266e408e
Николай Сотула <sotula@itstep1.dp.ua>
2019-10-27 20:25:44 +0200

    ajax post 1

--- 20191027_Ajax_again/app/example.js
+++ 20191027_Ajax_again/app/example.js
@@ -17,6 +17,16 @@ const dom = {};
 document.addEventListener('DOMContentLoaded', ()=>{
     dom.info = document.querySelector('.info');
     dom.btnGet = document.querySelector('.btn_get');
+    dom.btnPost = document.querySelector('.btn_post');
     dom.btnGet.addEventListener('click', get);
+    dom.btnPost.addEventListener('click', post);
 });
 
+function post(){
+    const api = 'http://so2niko.zzz.com.ua/examples/chat/api.php';
+    const aja = new XMLHttpRequest();
+
+    aja.open('post', api, true);
+    aja.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    aja.send('data=true&name=admin&color=#0000ff&text=admintext');
+}
