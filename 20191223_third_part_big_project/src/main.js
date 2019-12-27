00108df3318866970cda5ab38fda168c1c9cdc72
Nick Sotula <so2niko@gmail.com>
2019-12-27 21:17:46 +0200

    workin on spamer component

--- 20191223_third_part_big_project/src/main.js
+++ 20191223_third_part_big_project/src/main.js
@@ -2,9 +2,11 @@ import { ControllerAuthorization } from "./authorization/ControllerAuthorization
 import { Publisher } from "./share/Publisher.js";
 import { ControllerMenu } from "./menu/ControllerMenu.js";
 import { ControllerMessages } from "./messages/ControllerMessages.js";
+import { ControllerSpamer } from "./spamer/ControllerSpamer.js";
 
 const publisher = new Publisher();
 const methods = publisher.getMethods();
 const auth = new ControllerAuthorization(methods);
 const menu = new ControllerMenu(methods);
 const messages = new ControllerMessages(methods);
+const spamer = new ControllerSpamer(methods);
